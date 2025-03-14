fef542b4a36cc8cf8431c1e7555146a884ce9a68
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-14 21:17:16 +0100

    New test

---
+++ src/Dzaba.TeamCitySimulator.Lib.Tests/SimulationTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoFixture;
+using Dzaba.TeamCitySimulator.Lib.Model;
+using Dzaba.TestUtils;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Dzaba.TeamCitySimulator.Lib.Tests;
+
+[TestFixture]
+public class SimulationTests
+{
+    private IFixture fixture;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixture = TestFixture.Create();
+    }
+
+    private Simulation CreateSut()
+    {
+        return fixture.Create<Simulation>();
+    }
+
+    [Test]
+    public void Run_WhenOneBuild_ThenTwoEvents()
+    {
+        var settings = new SimulationSettings
+        {
+            Agents = [
+                new Agent
+                {
+                    Name = "TestAgent1"
+                }
+            ],
+            BuildConfigurations = [
+                new Build
+                {
+                    Name = "BuildConfig1",
+                    CompatibleAgents = ["TestAgent1"],
+                    Duration = TimeSpan.FromMinutes(1)
+                }
+            ],
+            QueuedBuilds = [
+                new QueuedBuild
+                {
+                    Name = "BuildConfig1",
+                    BuildsToQueue = 1
+                }
+            ]
+        };
+
+        var sut = CreateSut();
+
+        var result = sut.Run(settings).ToArray();
+        result.Should().HaveCount(2);
+    }
+}

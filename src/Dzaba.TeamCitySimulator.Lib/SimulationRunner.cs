e858585ba9b28dc14ddd26e97462daef021dff54
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-14 19:20:21 +0100

    Cache

--- src/Dzaba.TeamCitySimulator.Lib/SimulationRunner.cs
+++ src/Dzaba.TeamCitySimulator.Lib/SimulationRunner.cs
@@ -1,6 +1,7 @@
 ﻿using Dzaba.TeamCitySimulator.Lib.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Dzaba.TeamCitySimulator.Lib;
 
@@ -8,12 +9,17 @@ internal sealed class SimulationRunner
 {
     private readonly SimulationSettings simulationSettings;
     private readonly DateTime startDate = new DateTime(2025, 1, 1);
+    private readonly IReadOnlyDictionary<string, Build> buildsCached;
+    private readonly IReadOnlyDictionary<string, Agent> agentsCached;
 
     public SimulationRunner(SimulationSettings simulationSettings)
     {
         ArgumentNullException.ThrowIfNull(simulationSettings, nameof(simulationSettings));
 
         this.simulationSettings = simulationSettings;
+
+        buildsCached = simulationSettings.BuildConfigurations.ToDictionary(x => x.Name, x => x, StringComparer.OrdinalIgnoreCase);
+        agentsCached = simulationSettings.Agents.ToDictionary(x => x.Name, x => x, StringComparer.OrdinalIgnoreCase);
     }
 
     public IEnumerable<EventData> Run()

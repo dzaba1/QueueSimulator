707515574e3d7e5e54ba623420b967f5aebd9ff3
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-17 20:55:47 +0100

    Some first integration test

---
+++ src/Dzaba.QueueSimulator.WebApi.Tests/Integration/ControllerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace Dzaba.QueueSimulator.WebApi.Tests.Integration;
+
+public class ControllerTestFixture
+{
+    private WebApplicationFactory<Program> factory;
+
+    [SetUp]
+    public void SetupFactory()
+    {
+        factory = new WebApplicationFactory<Program>();
+    }
+
+    protected HttpClient CreateClient()
+    {
+        return factory.CreateClient();
+    }
+}
---
+++ src/Dzaba.QueueSimulator.WebApi.Tests/Integration/SimulateControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzaba.QueueSimulator.WebApi.Tests.Integration;
+
+[TestFixture]
+public class SimulateControllerTests : ControllerTestFixture
+{
+    [Test]
+    public async Task Csv_WhenSomeModel_ThenCsv()
+    {
+        var client = CreateClient();
+
+        var json = @"{
+}";
+
+        using var body = new StringContent(json, Encoding.UTF8, "application/json");
+
+        using var resp = await client.PostAsync("/simulate/csv", body);
+        resp.EnsureSuccessStatusCode();
+
+        var result = await resp.Content.ReadAsStringAsync();
+    }
+}

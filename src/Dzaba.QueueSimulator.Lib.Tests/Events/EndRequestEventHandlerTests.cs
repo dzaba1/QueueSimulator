d0292fa6dc4d47aa8f743a3f9b814ab08b023140
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-17 10:53:38 +0100

    More tests

---
+++ src/Dzaba.QueueSimulator.Lib.Tests/Events/EndRequestEventHandlerTests.cs
@@ -0,0 +1,133 @@
+﻿using AutoFixture;
+using Dzaba.QueueSimulator.Lib.Events;
+using Dzaba.QueueSimulator.Lib.Model;
+using Dzaba.QueueSimulator.Lib.Repositories;
+using Dzaba.TestUtils;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Dzaba.QueueSimulator.Lib.Tests.Events;
+
+[TestFixture]
+public class EndRequestEventHandlerTests
+{
+    private static readonly DateTime CurrentTime = DateTime.Now;
+
+    private IFixture fixture;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixture = TestFixture.Create();
+    }
+
+    private EndRequestEventHandler CreateSut()
+    {
+        return fixture.Create<EndRequestEventHandler>();
+    }
+
+    [Test]
+    public void Handle_WhenHandled_ThenAgentAndRequestIsFinished()
+    {
+        var eventData = new EventData("Test", CurrentTime);
+        var settings = new SimulationSettings
+        {
+            Agents = [
+                new AgentConfiguration
+                {
+                    Name = "Agent1"
+                }
+            ],
+            RequestConfigurations = [
+                new RequestConfiguration
+                {
+                    Name = "BuildConfig",
+                    Duration = TimeSpan.FromHours(1)
+                }
+            ],
+        };
+        fixture.SetupSimulationContext(settings);
+
+        var agent = new Agent
+        {
+            Id = 1,
+            AgentConfiguration = "Agent1"
+        };
+
+        var request = new Request
+        {
+            Id = 1,
+            AgentId = agent.Id,
+            RequestConfiguration = settings.RequestConfigurations[0].Name
+        };
+
+        fixture.FreezeMock<IAgentsRepository>()
+            .Setup(x => x.GetAgent(agent.Id))
+            .Returns(agent);
+
+        var sut = CreateSut();
+
+        sut.Handle(eventData, request);
+
+        agent.State.Should().Be(AgentState.Finished);
+        agent.EndTime.Should().Be(CurrentTime);
+        request.State.Should().Be(RequestState.Finished);
+        request.EndTime.Should().Be(CurrentTime);
+    }
+
+    [Test]
+    public void Handle_WhenHandled_ThenWaitingForAgentRequestsAreRescheduled()
+    {
+        var eventData = new EventData("Test", CurrentTime);
+        var settings = new SimulationSettings
+        {
+            Agents = [
+                new AgentConfiguration
+                {
+                    Name = "Agent1"
+                }
+            ],
+            RequestConfigurations = [
+                new RequestConfiguration
+                {
+                    Name = "BuildConfig",
+                    Duration = TimeSpan.FromHours(1)
+                }
+            ],
+        };
+        fixture.SetupSimulationContext(settings);
+
+        var agent = new Agent
+        {
+            Id = 1,
+            AgentConfiguration = "Agent1"
+        };
+
+        var request = new Request
+        {
+            Id = 1,
+            AgentId = agent.Id,
+            RequestConfiguration = settings.RequestConfigurations[0].Name
+        };
+
+        fixture.FreezeMock<IAgentsRepository>()
+            .Setup(x => x.GetAgent(agent.Id))
+            .Returns(agent);
+
+        var watingRequests = fixture.CreateMany<Request>(3).ToArray();
+        fixture.FreezeMock<IRequestsRepository>()
+            .Setup(x => x.GetWaitingForAgents())
+            .Returns(watingRequests);
+
+        var eventsPump = fixture.FreezeMock<ISimulationEventQueue>();
+
+        var sut = CreateSut();
+
+        sut.Handle(eventData, request);
+
+        eventsPump.Verify(x => x.AddCreateAgentQueueEvent(It.Is<Request>(r => watingRequests.Contains(r)), CurrentTime), Times.Exactly(3));
+    }
+}

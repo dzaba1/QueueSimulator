2f2b75c6e6d68bc86ee67c8aba1508d8bc0a2192
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-17 12:31:36 +0100

    More comparers

---
+++ src/Dzaba.QueueSimulator.Lib/Model/OnePropertyComparer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dzaba.QueueSimulator.Lib.Model;
+
+public class OnePropertyComparer<T, TKey> : IEqualityComparer<T>
+{
+    private readonly IEqualityComparer<TKey> keyComparer;
+    private readonly Func<T, TKey> keySelector;
+
+    public OnePropertyComparer(Func<T, TKey> keySelector,
+        IEqualityComparer<TKey> keyComparer)
+    {
+        ArgumentNullException.ThrowIfNull(keySelector, nameof(keySelector));
+        ArgumentNullException.ThrowIfNull(keyComparer, nameof(keyComparer));
+
+        this.keySelector = keySelector;
+        this.keyComparer = keyComparer;
+    }
+
+    public bool Equals(T? x, T? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x == null || y != null)
+        {
+            return false;
+        }
+
+        if (x != null || y == null)
+        {
+            return false;
+        }
+
+        return keyComparer.Equals(keySelector(x), keySelector(y));
+    }
+
+    public int GetHashCode([DisallowNull] T obj)
+    {
+        return keyComparer.GetHashCode(keySelector(obj));
+    }
+}
--- src/Dzaba.QueueSimulator.Lib/Model/StringPropertyEqualityComparer.cs
+++ src/Dzaba.QueueSimulator.Lib/Model/StringPropertyEqualityComparer.cs
@@ -1,53 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
-namespace Dzaba.QueueSimulator.Lib.Model
+namespace Dzaba.QueueSimulator.Lib.Model;
+
+public class StringPropertyEqualityComparer<T> : OnePropertyComparer<T, string>
 {
-    public sealed class StringPropertyEqualityComparer<T> : IEqualityComparer<T>
+    public StringPropertyEqualityComparer(Func<T, string> keySelector)
+        : this(keySelector, StringComparer.CurrentCulture)
     {
-        private readonly StringComparer stringComparer;
-        private readonly Func<T, string> keySelector;
 
-        public StringPropertyEqualityComparer(Func<T, string> keySelector)
-            : this(keySelector, StringComparer.CurrentCulture)
-        {
+    }
 
-        }
-
-        public StringPropertyEqualityComparer(Func<T, string> keySelector,
-            StringComparer stringComparer)
-        {
-            ArgumentNullException.ThrowIfNull(keySelector, nameof(keySelector));
-            ArgumentNullException.ThrowIfNull(stringComparer, nameof(stringComparer));
-
-            this.keySelector = keySelector;
-            this.stringComparer = stringComparer;
-        }
-
-        public bool Equals(T? x, T? y)
-        {
-            if (ReferenceEquals(x, y))
-            {
-                return true;
-            }
-
-            if (x == null || y != null)
-            {
-                return false;
-            }
-
-            if (x != null || y == null)
-            {
-                return false;
-            }
-
-            return stringComparer.Equals(keySelector(x), keySelector(y));
-        }
-
-        public int GetHashCode([DisallowNull] T obj)
-        {
-            return stringComparer.GetHashCode(keySelector(obj));
-        }
+    public StringPropertyEqualityComparer(Func<T, string> keySelector,
+        StringComparer stringComparer)
+        : base(keySelector, stringComparer)
+    {
+        
     }
 }

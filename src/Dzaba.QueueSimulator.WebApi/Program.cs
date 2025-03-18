484910d4f0196a65fe514f383cc8f4b5fcd17f1a
dzaba1 <blazej.zabialowicz@gmail.com>
2025-03-18 10:55:08 +0100

    Serilog

--- src/Dzaba.QueueSimulator.WebApi/Program.cs
+++ src/Dzaba.QueueSimulator.WebApi/Program.cs
@@ -2,11 +2,26 @@ using Dzaba.QueueSimulator.Lib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
+using System;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.RegisterDzabaQueueSimulatorLib();
+
+var logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"logs\QueueSimulator.log");
+var outputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u3}] ({SourceContext}) [{ThreadId}] {Message:lj}{NewLine}{Exception}";
+
+var logger = new LoggerConfiguration()
+    .Enrich.WithThreadId()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(LogEventLevel.Information, outputTemplate: outputTemplate)
+    .WriteTo.File(logFile, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, outputTemplate: outputTemplate)
+    .CreateLogger();
+builder.Services.AddLogging(l => l.AddSerilog(logger, true));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
